f781b73103cd748242ed936a6115f5ece9025621
jyjeon1225 <jyjeon1225@snu.ac.kr>
2024-06-07 20:54:16 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int N, M;
 int arr[9] = {0,};
-bool visited[9] = {0,};
+bool count[9] = {0,};
 
 void func(int cnt) {
     if(cnt == M) {
@@ -14,11 +14,11 @@ void func(int cnt) {
         return;
     }
     for(int i = 1; i <= N; i++) {
-        if(!visited[i]) {
-            visited[i] = true;
+        if(!count[i]) {
+            count[i] = true;
             arr[cnt] = i;
             func(cnt + 1);
-            visited[i] = false;
+            count[i] = false;
         }
     }
 }
