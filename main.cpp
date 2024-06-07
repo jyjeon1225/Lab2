65884382cfb418cb8b43d658f8dd20f867d3cd5b
jyjeon1225 <jyjeon1225@snu.ac.kr>
2024-06-07 20:52:57 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,20 +3,27 @@
 
 using namespace std;
 
-int N,M;
-vector<int> vec;
+int N, M;
+int arr[9] = {0,};
+bool visited[9] = {0,};
 
 void func(int cnt) {
-
-
-
-
-
-
-
+    if(cnt == M) {
+        for(int i = 0; i < M; i++) cout << arr[i] << " ";
+        cout << endl;
+        return;
+    }
+    for(int i = 1; i <= N; i++) {
+        if(!visited[i]) {
+            visited[i] = true;
+            arr[cnt] = i;
+            func(cnt + 1);
+            visited[i] = false;
+        }
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+	cin >> N >> M;
+	func(0);
 }
